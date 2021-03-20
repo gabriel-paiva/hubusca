e5357c37b499c7b8ff9e97c7ba55370677ea357b
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-03-20 11:19:03 -0300

    feat: create home page search form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './App.css';
+import Logo from './assets/hubusca-logo.png';
 
 const App: React.FC = () => {
   console.log('Hello World');
   return (
     <div className="App">
-      Hello World!
+      <form>
+        <img src={Logo} alt="Hubusca Logo" />
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Digite o nome do usuário"
+        />
+        <div className="buttons">
+          <button id="search" type="submit">Pesquisar</button>
+          <button id="link" type="button">Ver Histórico</button>
+        </div>
+      </form>
     </div>
   );
 };
